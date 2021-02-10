08a7537cc029b13592a4499e9474cac91b365b4b
garegin.ayvazyan <garegin.ayvazyan@outlook.com>
2021-02-10 16:11:41 +0400

    10.02.2021

---
+++ CecSessions/CecSessions.UI/Pages/admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CecSessions.UI.Pages.admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
